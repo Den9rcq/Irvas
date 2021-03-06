608ea0ac664de2d845d4fc90cc672e82d8a349e1
Den9 <den9.rcq@gmail.com>
2021-03-06 23:27:34 +0300

    test

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,4 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click'); //^ .no_click единственный совпадающий класс у всех табов
-
 });
