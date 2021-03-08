05ae19b3a41696c0964f90589f0bbf0cb5961629
Den9 <den9.rcq@gmail.com>
2021-03-08 15:06:21 +0300

    modification of tabs & modals

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,6 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click'); //^ .no_click единственный совпадающий класс у всех табов
-    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'a');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms();
 });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,9 +1,10 @@
 const modals = () => {
 
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelector(closeSelector);
+            close = document.querySelector(closeSelector),
+            widows = document.querySelectorAll('[data-modal]');
 
         // Показ модального окна
         trigger.forEach(item => {
@@ -12,22 +13,32 @@ const modals = () => {
                     e.preventDefault();
                 }
 
+                // Скрываем не активные pop-up
+                widows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
             });
         });
 
-        // // Закрытие модального окна на крестик
-        // close.addEventListener('click', () => {
-        //     modal.style.display = 'none';
-        //     document.body.style.overflow = '';
-        // });
+        // Закрытие модального окна на крестик
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        });
 
         // Закрытие модального окна на подложку и крестик
         modal.addEventListener('click', (e) => {
-            if (e.target === modal || e.target === close || e.target === close.firstChild) {
+            if (e.target === modal && closeClickOverlay) {
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
+
+                // Закрытие всех pop-up сразу
+                widows.forEach(item => {
+                    item.style.display = 'none';
+                });
             }
         });
     }
@@ -43,6 +54,8 @@ const modals = () => {
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
     showModalByTime('.popup', 60000);
 
 };
