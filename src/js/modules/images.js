a90ff20ddfe3095cd89de200ed82beaee9ca13bc
Den9 <den9.rcq@gmail.com>
2021-03-11 20:53:33 +0300

    added images script

---
+++ src/js/modules/images.js
@@ -0,0 +1,32 @@
+const images = () => {
+    const imgPopup = document.createElement('div'),
+        bigImage = document.createElement('img'),
+        workSection = document.querySelector('.works');
+
+    imgPopup.classList.add('popup');
+    workSection.appendChild(imgPopup);
+    imgPopup.style.cssText = `
+        justify-content: center;
+        align-items: center;
+        display: none;
+  `;
+    imgPopup.appendChild(bigImage);
+    bigImage.style.maxwidth = '50%';
+    bigImage.style.maxheight = '50%';
+
+    workSection.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target && e.target.classList.contains('preview')) {
+            imgPopup.style.display = 'flex';
+            const path = e.target.parentNode.getAttribute('href');
+            bigImage.setAttribute('src', path);
+        }
+
+        if (e.target && e.target.matches('div.popup')) { //^ matches - совпадает
+            imgPopup.style.display = 'none';
+        }
+    });
+
+};
+
+export default images;
